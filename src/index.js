01be768577d89a6a0254fc7aa4ce2ad5ab8c6e8f
DavidRZuluaga <git@davidrzuluaga.com>
2018-11-12 22:17:14 -0500

    ejercicio

--- src/index.js
+++ src/index.js
@@ -5,9 +5,15 @@ import {App, Page1, Page2, NotFound} from './App';
 import './index.css';
 
 
-// Implementa las rutas aca
-
 ReactDOM.render(
-  <App />, // Es posible que tengas que hacer un cambio en esta linea.
+  <App >
+  <Router history={hashHistory}>
+    <Route path="/" component={Page1} />
+    <Route path="Page1" component={Page1} />
+    <Route path="Page2" component={Page2} />
+    <Route path="*" component={NotFound} />
+  </Router>
+
+   </App>, // Es posible que tengas que hacer un cambio en esta linea.
   document.getElementById('root')
 );
